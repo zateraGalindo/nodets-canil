c260c1ed2d1a0b9e0dfcb51891c7c8f9872755bd
Gabriel Galindo <zatera_@hotmail.com>
2022-07-01 18:29:58 -0300

    final project 1.0

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,21 +1,56 @@
 import { Request, Response } from 'express';
 
+import { Pet } from '../models/pet';
+import {createMenuObject} from '../helpers/createMenuObject';
+
 export const home = (req:Request, res:Response) => {
-    res.send('Home no Controller');
-    // res.render('pages/page');
+    let list = Pet.getAll();
+
+    res.render('pages/page', {
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        },
+        list
+    });
 }
 
 export const dogs = (req:Request, res:Response) => {
-
-    // res.render('pages/dogs');
+    let list = Pet.getFromType('dog');
+    
+    res.render('pages/page', {
+        menu: createMenuObject('dog'),
+        banner: {
+            title: 'Todos os cachorros',
+            background: 'banner_dog.jpg'
+        },
+        list
+    });
 }
 
 export const cats = (req:Request, res:Response) => {
+    let list = Pet.getFromType('cat');
 
-    // res.render('pages/cats');
+    res.render('pages/page', {
+        menu: createMenuObject('cat'),
+        banner: {
+            title: 'Todos os gatos',
+            background: 'banner_cat.jpg'
+        },
+        list
+    });
 }
 
 export const fishes = (req:Request, res:Response) => {
+    let list = Pet.getFromType('fish');
 
-    // res.render('pages/fishes');
+    res.render('pages/page', {
+        menu: createMenuObject('fish'),
+        banner: {
+            title: 'Todos os peixes',
+            background: 'banner_fish.jpg'
+        },
+        list
+    });
 }
--- src/server.ts
+++ src/server.ts
@@ -12,13 +12,13 @@ server.set('view engine', 'mustache');
 server.set('views', path.join(__dirname, 'views'));
 server.engine('mustache', mustache());
 
-server.use(express.static(path.join(__dirname, '../publi')));
+server.use(express.static(path.join(__dirname, '../public')));
 
 //Rotas
 server.use(mainRoutes);
 
 server.use((req, res)=>{
-    res.send('Página não encontrada!!!');
+    res.render('pages/404');
 });
 
 
